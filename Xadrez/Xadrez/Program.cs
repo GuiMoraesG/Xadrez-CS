a17feaf2172799d229079e2abf340a2af4e7354f
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-17 15:21:00 -0300

    Adicionado as classes do tabuleiro

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -7,13 +7,11 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Posicao p;
+            Tabuleiro tab;
 
-            p = new Posicao(3, 4);
+            tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(p);
-
-            Console.ReadLine();
+            Console.WriteLine(tab.Colunas);
         }
     }
 }
---
+++ Xadrez/Xadrez/tabuleiro/Cor.cs
@@ -0,0 +1,8 @@
+﻿namespace Xadrez.tabuleiro
+{
+    internal enum Cor
+    {
+        Branca,
+        Preta,
+    }
+}
