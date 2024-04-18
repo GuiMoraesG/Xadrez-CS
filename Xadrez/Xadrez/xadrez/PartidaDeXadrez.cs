5eaa088525cf56695d95abcfc0da494886896c93
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-18 20:29:21 -0300

    Correção

--- Xadrez/Xadrez/xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/xadrez/PartidaDeXadrez.cs
@@ -105,7 +105,7 @@ namespace Xadrez.xadrez
 
         public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
         {
-            if (!Tab.Peca(origem).PodeMoverPara(destino))
+            if (!Tab.Peca(origem).MovimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição inválida");
             }
